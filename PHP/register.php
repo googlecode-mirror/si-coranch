810f171e4367c870e71e0490d84603f01749cd48
themhd17 <themhd17@gmail.com>
2012-11-26 09:53:17 +0000

    Pembuatan tampilan register.php dan logout.php

---
+++ PHP/register.php
@@ -0,0 +1,98 @@
+<html xmlns="http://www.w3.org/1999/xhtml" class="no-js
+
+    <head>
+         <link href="//gathercontent.com/skin/fullwidthtest/css/global/_dynamic.php?v=1347283090" rel="stylesheet" media="screen" />
+<link href="//gathercontent.com/skin/fullwidthtest/css/signup/_dynamic.php?v=1347283101" rel="stylesheet" media="screen" />
+
+
+<link rel="shortcut icon"  href="//gathercontent.com/skin/images/gathercontent-fav-16px-2.png">
+    </head>
+    <body>
+        <div class="notification-bar ajax-notification-bar"><button class="close">&times;</button></div>         <div class="wrapper ajax-wrapper">
+
+
+
+
+<div class="client-banner cf">
+    <div class="container">
+        <div class="row">
+            <div class="inner-wrapper cf span12">
+                <div class="left">
+                    <a href="//gathercontent.com/">
+                        <div class="gather-logo">
+                            <div>
+                                <img src="//gathercontent.com/skin/images/gathercontent-logo-inverted.png">
+                            </div>
+                        </div>
+                    </a>
+                    
+                </div>
+                <div class="right micro-text">
+                    Your new account, step 1 of 1                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="signup-wrapper cf">
+    <div class="form-wrapper left">
+        <div class="well-white bottom20px">
+            <div class="padding20px form">
+                <form action="//gathercontent.com/signup/submit" method="post" accept-charset="utf-8" class="kissmetrics-signup">
+                <div class="cf small-inputs">
+                    <div class="left">
+                        <label>First Name</label>
+                        <input type="text" name="first_name" value=""  />                    </div>
+
+                    <div class="right">
+                        <label>Last Name</label>
+                        <input type="text" name="last_name" value=""  />                    </div>
+                </div>
+
+                <label>Company or organisation</label><input type="text" name="company_name" value=""  /><label>Email address</label><input type="text" name="email" value="" class="ajax-email " /><label>Password</label><input type="password" name="password" value=""  /><div class="urlkey-wrapper"></div>                <div class="cf">
+                    <div class="left"></div>
+                    <div class="right">
+                        <input type="submit" class="btn btn-success btn-large" value="Get started">
+                    </div>
+                </div>
+                </form>                <span class="by-clicking">By clicking "Get Started" you agree to <a href="//gathercontent.com/terms-of-use">Terms of Use</a> and <a href="//gathercontent.com/privacy-policy">Privacy Policy</a>.</span>
+            </div>
+        </div>
+    </div>
+    <div class="perks right"></div>
+    
+</div>
+
+</div>
+<script type="text/javascript" src="//gathercontent.com/skin/fullwidthtest/js/global/_dynamic.php?v=1347028498"></script>
+<script type="text/javascript" src="//gathercontent.com/skin/fullwidthtest/js/signup/_dynamic.php?v=1346756974"></script>
+
+
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-24262483-1']);
+  _gaq.push(['_setDomainName', 'gathercontent.com']);
+      _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+<script type="text/javascript">
+  var _kmq = _kmq || [];
+  function _kms(u){
+    setTimeout(function(){
+    var s = document.createElement('script'); var f = document.getElementsByTagName('script')[0]; s.type = 'text/javascript'; s.async = true;
+    s.src = u; f.parentNode.insertBefore(s, f);
+    }, 1);
+  }
+  _kms('//i.kissmetrics.com/i.js');_kms('//doug1izaerwt3.cloudfront.net/f9fe74a21c8c6c385e35f0ded1acb12750aa568c.1.js');
+</script>
+
+</body></html>

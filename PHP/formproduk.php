21cb696eb556b8986ce0ba5ce5fddb1862153adf
themhd17 <themhd17@gmail.com>
2013-01-08 06:00:16 +0000

    update form produk

--- PHP/formproduk.php
+++ PHP/formproduk.php
@@ -5,6 +5,8 @@ include"page_koperasi.php";
 $sql="SELECT * FROM produk WHERE id_produk=".$_GET['id'];
 $edit = mysql_query($sql);
 $a = mysql_fetch_array($edit);
+$akun= mysql_query("SELECT*FROM koperasi where id_koperasi=".$_SESSION['id_koperasi']);
+while($akun1= mysql_fetch_array($akun)){
 ?>
 <html>
 <head>
@@ -20,19 +22,62 @@ $a = mysql_fetch_array($edit);
 	  }
 	</style>
 	<link href="css/bootstrap-responsive.css" rel="stylesheet">
-	<link href="css/lala.css" rel="stylesheet">
+	<link href="css/home.css" rel="stylesheet">
+	<link href="css/home1.css" rel="stylesheet">
 	<link rel="shortcut icon" href="img/favicon.ico">		
 </head>
 
 <body>
+	<div class="navbar">
+		<div class="navbar-inner">
+			<div class="container-fluid">
+				<a class="brand" href="index1.php"> <img alt="SI-Coranch Logo" src="img/Favicon.png" /> <span>SI-Coranch</span></a>				
+				<div class="btn-group pull-right" >
+					<a class="btn btn-medium" href="logout_koperasi.php">
+						<i class="icon-user"></i><span class="hidden-phone"> Logout</span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	
 		<div class="container-fluid">
 		<div class="row-fluid">
 
-			<div id="content" class="span5">
+			<div class="span2 main-menu-span">
+				<div class="well nav-collapse sidebar-nav">
+					<ul class="nav nav-tabs nav-stacked main-menu">
+						<li class="nav-header hidden-tablet">Selamat Datang, <?php echo $akun1['nama_koperasi']; ?> !</li>
+						
+						<li><a href="viewproduk.php"><i class="icon-tasks"></i><span class="hidden-tablet"> Produk 
+						<ul><a class="" href="tmbhproduk.php"><i class="icon-Pencil"></i><span class="hidden-tablet" role = "treeitem"> Tambah Produk </span></a></ul>
+						</span></a>
+						</li>
+						
+						<li><a class="" href="profil_koperasi.php"><i class="icon-user"></i><span class="hidden-tablet"> Profil </span></a></li>
+					</ul>
+				</div>
+			</div>
+		
+			<div id="content" class="span10">			
+                <ul class="breadcrumb"> 
+					<li>
+						<a href="index1.php">Home </a> <span class="divider">/</span>
+					</li>
+					<li>
+						<a href="viewproduk.php"> Daftar Produk </a> <span class="divider">/</span>
+					</li>
+					<li>
+						<a>Edit Produk</a>
+					</li>
+				</ul>
+			</div>
+		
+			<div id="content" class="span10">
 			<div class="row-fluid sortable">
 				<div class="box span12">
 					<div class="breadcrumb">
-						<h2><i class="icon-picture"></i>EditProduk</h2>
+						<h2><i class="icon-Pencil"></i>EditProduk</h2>
 					</div>
 
 					<div class="box-content">
@@ -151,3 +196,4 @@ $a = mysql_fetch_array($edit);
 	</div>
 </body>
 </html>
+<?}?>
